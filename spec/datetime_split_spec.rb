a48cf33a3acf31e156e59b27456fac4a480c8d50
eddie <eddie@visionbundles.com>
2013-09-29 13:21:32 +0800

    Add datetime split spec

---
+++ spec/datetime_split_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "[Datetime Split] test" do
+  before :all do
+    @user = FactoryGirl.create(:user)
+    @user.confirm_datetime_date = "2013-10-1"
+    @user.confirm_datetime_time = "10:20"
+  end
+
+  context "Getter" do
+    it "confirm_datetime should be 2013/10/1 10:20" do
+      @user.confirm_datetime.to_date.should == Date.parse("2013/10/1 10:20")
+    end
+
+    it "confirm_datetime_date should be same after convert" do
+      Date.parse(@user.confirm_datetime_date).should == Date.parse("2013/10/01")
+    end
+
+    it "confirm_datetime_date_time should be same after convert" do
+      Time.parse(@user.confirm_datetime_time).should == Time.parse("10:20")
+    end
+  end
+end
